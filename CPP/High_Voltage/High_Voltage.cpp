5b8b2e0d4ac0df967bb9cc21bf3ed7a1ad6bb348
Erik J <ejantz@willdan.com>
2024-05-10 20:08:51 -0500

    Add Cpp Initial Files

---
+++ CPP/High_Voltage/High_Voltage.cpp
@@ -0,0 +1,12 @@
+﻿// High_Voltage.cpp : Defines the entry point for the application.
+//
+
+#include "High_Voltage.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
